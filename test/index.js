ae89cd231933d01237abd87ef1217b0c1faff827
Tyler Benton <tjbenton21@gmail.com>
2017-02-06 12:50:16 -0500

    added additional tests for new features
    Also updated existing tests to use a delay function to simplify them.

--- test/index.js
+++ test/index.js
@@ -2,6 +2,12 @@
 var assert = require('assert');
 var EventEmitter = require('../');
 
+function delay (time) {
+	return new Promise(function (resolve) {
+		setTimeout(resolve, time || 100);
+	});
+}
+
 describe('AsyncEventEmitter', function () {
 	for (var i = 0; i < 5; i++) {
 		(function (i) {
@@ -10,11 +16,7 @@ describe('AsyncEventEmitter', function () {
 
 				ee.on('evt', function () {
 					arguments.length && assert(arguments[i - 1] === i);
-					return new Promise(function (resolve) {
-						setTimeout(function () {
-							resolve();
-						}, 10);
-					});
+					return delay(10);
 				});
 
 				var args = ['evt'];
@@ -36,19 +38,11 @@ describe('AsyncEventEmitter', function () {
 
 				ee.on('evt', function () {
 					arguments.length && assert(arguments[i - 1] === i);
-					return new Promise(function (resolve) {
-						setTimeout(function () {
-							resolve();
-						}, 10);
-					});
+					return delay(10);
 				});
 				ee.on('evt', function () {
 					arguments.length && assert(arguments[i - 1] === i);
-					return new Promise(function (resolve) {
-						setTimeout(function () {
-							resolve();
-						}, 10);
-					});
+					return delay(10);
 				});
 
 				var args = ['evt'];
@@ -93,5 +87,86 @@ describe('AsyncEventEmitter', function () {
 		assert.throws(function () {
 			ee.emit('error');
 		}, /Uncaught/);
+	});
+
+	it('should catch error and return promise when a error constructor is passed', function (done) {
+		var ee = new EventEmitter();
+		ee.emit({ catch: true }, 'error', new Error('foo'))
+			.then(function () {
+				assert.fail('should have thrown an error');
+				done();
+			})
+			.catch(function (err) {
+				assert.ok(/Error: foo/.test(err));
+				done();
+			});
+	});
+
+	it('should catch error and return promise when an uncaught error occurs', function (done) {
+		var ee = new EventEmitter();
+		ee.emit({ catch: true }, 'error')
+			.then(() => {
+				assert.fail('should have thrown an error');
+				done();
+			})
+			.catch((err) => {
+				assert.ok(/Uncaught/.test(err));
+				done();
+			});
+	});
+
+	it('should emit events serially', function (done) {
+		var actual = [];
+		var expected = [ 40, 30, 20, 10 ];
+		var ee = new EventEmitter();
+
+		[ 40, 30, 20, 10 ].forEach(function (n) {
+			ee.on('test', () => {
+				return delay(n)
+				.then(function () {
+					actual.push(n);
+					return n;
+				});
+			});
+		});
+
+		ee.emit({ series: true }, 'test')
+			.then(function (result) {
+				assert.deepEqual(result, expected);
+				assert.deepEqual(actual, expected);
+				done();
+			})
+			.catch(function (err) {
+				assert.fail(err);
+				done();
+			});
+	});
+
+	it('should emit events async', function (done) {
+		var actual = [];
+		var expected = [ 10, 20, 30, 40 ];
+		var ee = new EventEmitter();
+
+		[ 40, 30, 20, 10 ].forEach(function (n) {
+			ee.on('test', () => {
+				return delay(n)
+				.then(function () {
+					actual.push(n);
+					return n;
+				});
+			});
+		});
+
+		ee.emit('test')
+			.then(function (result) {
+				assert.deepEqual(actual, expected);
+				// result should still be the same order as it was called
+				assert.deepEqual(result.reverse(), expected);
+				done();
+			})
+			.catch(function (err) {
+				assert.fail(err);
+				done();
+			});
 	});
 });
